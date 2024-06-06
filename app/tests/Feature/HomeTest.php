bfbef68433617ec6ef53057fccf0a464c89f9af8
sabri taieb <sabritaiebdev@gmail.com>
2024-06-06 14:41:36 +0100

    Rename HomeControllerTest to HomeTest

--- app/tests/Feature/HomeTest.php
+++ app/tests/Feature/HomeTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Feature;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class HomeControllerTest extends WebTestCase
+class HomeTest extends WebTestCase
 {
     public function testSomething(): void
     {
